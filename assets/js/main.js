23a1e01ef82dec03ad830b00533c0d7905bbb47f
alimranhung <42710065+alimranhung@users.noreply.github.com>
2018-09-02 15:07:13 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($){
+	$('#clock').countdown('2019/01/01', function(event) {
+		  $(this).html(event.strftime('<span class="countdown-wrap"><span class="single-countdown-item">%D <br/><span>days</span></span><span class="single-countdown-item">%H <br/><span>hours</span></span><span class="single-countdown-item">%M <br/><span>minutes</span></span><span class="single-countdown-item">%S <br/><span>seconds</span></span></span>'));
+		});
+});
